b4533137067130ca802b9f982d672fe3747e7988
Luka Mirosevic <luka@goonbee.com>
2013-07-09 16:48:24 +0200

    made it into a project with iOS and OSX target libraries

---
+++ JRSwizzle-OSX/JRSwizzle_OSX.h
@@ -0,0 +1,13 @@
+//
+//  JRSwizzle_OSX.h
+//  JRSwizzle-OSX
+//
+//  Created by Luka Mirosevic on 09/07/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JRSwizzle_OSX : NSObject
+
+@end
---
+++ JRSwizzle-OSX/JRSwizzle_OSX.m
@@ -0,0 +1,13 @@
+//
+//  JRSwizzle_OSX.m
+//  JRSwizzle-OSX
+//
+//  Created by Luka Mirosevic on 09/07/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "JRSwizzle_OSX.h"
+
+@implementation JRSwizzle_OSX
+
+@end
